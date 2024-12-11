5b3f08ec8274faee2da2c0a155d04630ed89da50
jemalmohcine <mohcine.jemal@gmail.com>
2024-12-11 12:01:55 +0100

    add hello text

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ app.use(bodyParser.json());
 // Routes
 app.use('/appointments', appointmentsRoutes);
 
+// New route for "Hello autodealersdigital"
+app.get('/', (req, res) => {
+  res.send('Hello autodealersdigital');
+});
+
 // Server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
